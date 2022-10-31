d04980038230d4e72756955d5987789cb9f361db
Duncan Caruthers <duncan.caruthers01@gmail.com>
2022-10-31 16:12:35 -0400

    Post Model added

---
+++ backend/Models/Post.cs
@@ -0,0 +1,10 @@
+namespace Models;
+
+public class Post
+{
+    public Guid id { get; set; }
+    public Guid sender_id { get; set; }
+    public Guid reciver_id { get; set; }
+    public string text { get; set; } = "";
+    public DateTime time { get; set; }
+}
