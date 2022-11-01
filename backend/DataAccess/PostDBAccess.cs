1b6bc7352244120c3854701ab6f4389f8f0dc279
Duncan Caruthers <duncan.caruthers01@gmail.com>
2022-11-01 12:03:01 -0400

    Implemented PostDBAccess.Add(Post)

--- backend/DataAccess/PostDBAccess.cs
+++ backend/DataAccess/PostDBAccess.cs
@@ -1,5 +1,6 @@
 using Microsoft.Data.SqlClient;
 using Models;
+using Serilog;
 
 namespace DataAccess;
 
@@ -14,7 +15,37 @@ public class PostDBAccess : IDBAccess<Post>
 
     public Post Add(Post t)
     {
-        throw new NotImplementedException();
+        Post temp = new Post();
+
+        try
+        {
+            using SqlConnection connection = _factory.GetConnection();
+            connection.Open();
+
+            SqlCommand cmd = new SqlCommand(@"
+                insert into Posts (id,senderId,receiverId,text,time) 
+                    values (@id,@uid,@rid,@text,@time); 
+                select * from Posts where id = @id;", connection);
+            cmd.Parameters.AddWithValue("@id", temp.id);
+            cmd.Parameters.AddWithValue("@uid", t.sender_id);
+            cmd.Parameters.AddWithValue("@rid", t.reciver_id);
+            cmd.Parameters.AddWithValue("@text", t.text);
+            cmd.Parameters.AddWithValue("@time", temp.time);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                reader.Read();
+                temp.sender_id = (Guid)reader["senderId"];
+                temp.reciver_id = (Guid)reader["receiverId"];
+                temp.text = (string)reader["text"];
+            }
+        }
+        catch (SqlException e)
+        {
+            Log.Error("An exception was thrown while adding a user to the database", e);
+        }
+        return temp;
     }
 
     public Post Delete(Post t)
