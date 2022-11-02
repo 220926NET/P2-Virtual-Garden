dcc72c4619b8d70c936d828116b38719307d81d9
rushayhays <rushayhays@outlook.com>
2022-11-02 13:55:43 -0500

    additional component gardenGrid created and nested inside garde

---
+++ frontend/src/app/garden-grid/garden-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-garden-grid',
+  templateUrl: './garden-grid.component.html',
+  styleUrls: ['./garden-grid.component.css']
+})
+export class GardenGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
