f475ff225c7005eccb5fcd48cbbb4da87b2bcb6d
Jacob Allen <jallen1887@gmail.com>
2022-11-03 18:04:21 -0400

    Add salting and hashing to passwords, changing functionallity of user services

--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -3,6 +3,8 @@ namespace Models;
 public class User
 {
     public Guid id { get; set; } = Guid.NewGuid();
-    public string? username { get; set; } = "";
-    public string? password { get; set; } = "";
+    public string username { get; set; } = string.Empty;
+    public byte[] passwordHash { get; set; } = Array.Empty<byte>();
+    public byte[] passwordSalt { get; set; } = Array.Empty<byte>();
+    
 }
