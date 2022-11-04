8c025be02b2e72dab877d34da079eff9ac1b5fb4
Duncan Caruthers <duncan.caruthers01@gmail.com>
2022-11-04 18:47:10 -0400

    Fixed sonar cloud bug

--- backend/Models/FriendRelationshipValidator.cs
+++ backend/Models/FriendRelationshipValidator.cs
@@ -4,6 +4,6 @@ public class FriendRelationshipValidator : IValidate<FriendRelationship>
 {
     public bool isValid(FriendRelationship toCheck)
     {
-        return !string.IsNullOrEmpty(toCheck.username) && !string.IsNullOrEmpty(toCheck.username);
+        return !string.IsNullOrEmpty(toCheck.username) && !string.IsNullOrEmpty(toCheck.friendname);
     }
 }
