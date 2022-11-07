a9ee7cd9575597c1d50c82e2cdcb9adfa6caa9ea
rushayhays <rushayhays@outlook.com>
2022-11-06 23:38:51 -0600

    weather API calls successfully

--- frontend/src/app/core/weather.service.ts
+++ frontend/src/app/core/weather.service.ts
@@ -10,6 +10,13 @@ import { IForecast } from '../shared/interface';
 })
 export class WeatherService {
 
-  baseUrl: string = '';
-  constructor() { }
+  baseUrl: string = 'https://api.openweathermap.org/data/2.5/weather?';
+  constructor(private http: HttpClient) { }
+
+  getForecast() : Observable<IForecast>{
+    
+    var myForecast = this.http.get<IForecast>(this.baseUrl + 'lat=35.95&lon=-86.66&appid=f78b83839e67bea5a6c860811cef3a6d&units=imperial');
+    console.log(myForecast);
+    return myForecast;
+  }
 }
