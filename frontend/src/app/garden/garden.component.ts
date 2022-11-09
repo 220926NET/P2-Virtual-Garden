262c997e84d29718475b8d8b54df0767ada901d6
Duncan Caruthers <duncan.caruthers01@gmail.com>
2022-11-09 13:28:14 -0500

    Restored gargden.component.ts

--- frontend/src/app/garden/garden.component.ts
+++ frontend/src/app/garden/garden.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Guid } from 'guid-typescript';
-import { FriendService } from '../core/friend.service';
-import { GardenService } from '../core/garden.service';
-import { IFriendRelationship, IGarden } from '../shared/interface';
 import { WeatherComponent } from '../weather/weather.component';
 
 
@@ -14,24 +10,9 @@ import { WeatherComponent } from '../weather/weather.component';
 
 export class GardenComponent implements OnInit {
 
-  constructor(private gservice: GardenService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    let garden: IGarden = {
-      id: Guid.EMPTY,
-      user_id: "28e70132-9ae8-4cce-8566-712aead94e94",
-      tiles: []
-    }
-    console.log(JSON.stringify(garden));
-    this.gservice.addGarden(garden).subscribe((res) => {
-      this.gservice.getGarden(res.user_id).subscribe((res2) => {
-        this.gservice.updateGarden(res2).subscribe((res3) => {
-          this.gservice.deleteGarden(res3).subscribe((res4) => {
-            console.log(res4);
-          })
-        })
-      })
-    })
   }
 
 }
