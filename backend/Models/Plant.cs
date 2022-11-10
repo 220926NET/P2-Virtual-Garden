94c4e5d7b4a4e507ed98184ce0def47a8756e5d5
Duncan Caruthers <duncan.caruthers01@gmail.com>
2022-11-10 15:30:13 -0500

    Garden stuff added

--- backend/Models/Plant.cs
+++ backend/Models/Plant.cs
@@ -6,4 +6,6 @@ public class Plant
     public string name { get; set; } = "";
     public int growth_minuets { get; set; } = -1;
     public int worth { get; set; } = -1;
+    public string image_path { get; set; } = "";
+    public int state { get; set; } = -1;
 }
--- backend/Models/Tile.cs
+++ backend/Models/Tile.cs
@@ -5,7 +5,7 @@ public class Tile
     public Guid id { get; set; } = Guid.NewGuid();
     public Guid garden_id { get; set; } = Guid.Empty;
     public int position { get; set; } = -1;
-    public Guid plant_id { get; set; } = Guid.Empty;
+    public Plant plant_information { get; set; } = new();
     public DateTime plant_time { get; set; } = DateTime.Now;
     public DateTime ground_time { get; set; } = DateTime.Now;
 }
