1b240125fb7d9f814e3d5c0b80c0241fc1ada015
Duncan Caruthers <duncan.caruthers01@gmail.com>
2022-11-16 15:24:21 -0500

    Test config

--- frontend/src/app/core/post.service.ts
+++ frontend/src/app/core/post.service.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Params } from '@angular/router';
-import { Observable, throwError} from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { Guid } from 'guid-typescript';
 
 import { map, catchError } from 'rxjs/operators';
 import { IPost } from '../shared/interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -13,29 +14,29 @@ import { IPost } from '../shared/interface';
 
 export class PostService {
 
-  swaggerBaseUrl : string = "https://localhost:7077/api/"
+  swaggerBaseUrl: string = environment.baseApi;
 
   constructor(private http: HttpClient) { }
 
-  getAllPostsUserIsRecipientOf(userId:Guid): Observable<IPost[]>{
+  getAllPostsUserIsRecipientOf(userId: Guid): Observable<IPost[]> {
     return this.http.get<IPost[]>(`${this.swaggerBaseUrl}get/${userId}`)
-    .pipe(
-      catchError(this.handleError)
-    );
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 
-  sendPost(post:IPost) : Observable<Object>{
-    return this.http.post(this.swaggerBaseUrl + "post/",post);
+  sendPost(post: IPost): Observable<Object> {
+    return this.http.post(this.swaggerBaseUrl + "post/", post);
   }
 
-  private handleError(error:any){
+  private handleError(error: any) {
     console.error('server error:', error);
-    if(error.error instanceof Error){
+    if (error.error instanceof Error) {
       const errMessage = error.error.message;
       return throwError(() => new Error(errMessage));
 
     }
     return throwError(() => new Error(error || 'Node.js server error'));
   }
-  
+
 }
